20eb72a40875a53cb5fbda25394eff95e32cf5f5
Celisuis <rhian.macham@outlook.com>
2018-08-11 15:07:31 +0100

    Added Price Helper
    Add Price Helper Class with Return Gold Function (Version 1)

---
+++ Helpers/PriceHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSMCSharp.Helpers
+{
+    public class PriceHelper
+    {
+        public static string ReturnGoldValue(long value)
+        {
+            long valueDivided = value / 10000;
+
+            string output = String.Empty;
+
+            if (valueDivided >= 0 && valueDivided < 10000)
+                output = valueDivided.ToString() + "g";
+            if (valueDivided >= 10000 && valueDivided <= 100000)
+                output = valueDivided.ToString("##,###") + "g";
+            if (valueDivided >= 100000 && valueDivided < 1000000)
+                output = valueDivided.ToString("###,###") + "g";
+            if (valueDivided >= 1000000 && valueDivided < 10000000)
+                output = valueDivided.ToString("#,###,###") + "g";
+            if (valueDivided == 10000000)
+                output = valueDivided.ToString("##,###,###") + "g";
+
+            if (valueDivided > 10000000)
+                output = "ERROR Parsing Value";
+
+            return output;
+        }
+    }
+}
